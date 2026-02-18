80d0efe8c5d0d31aa87fa08a7f8df7bcd4f98d1d
RamssCR <ramsesgpm@gmail.com>
2026-02-18 10:53:52 -0500

    feat: created volume exercise (with class)

---
+++ include/volume_class.hpp
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <stdexcept>
+#include <cstring>
+
+class AudioPlayer {
+public:
+    explicit AudioPlayer(const int volume) : volume_(volume), buffer_(new char[1]{'m'}) {
+        if (volume < 0 || volume > 100) throw std::out_of_range("value out of range");
+    }
+
+    AudioPlayer(const AudioPlayer& other)
+    : volume_(other.volume_), buffer_(new char[std::strlen(other.buffer_)]) {
+        std::strcpy(buffer_, other.buffer_);
+    }
+
+    ~AudioPlayer();
+
+    AudioPlayer& operator=(const AudioPlayer& other);
+
+    [[nodiscard]] int volume() const;
+    [[nodiscard]] char buffer() const;
+
+    bool set_volume(int volume);
+
+    void max_volume();
+
+    void mute();
+
+private:
+    int volume_ = 0;
+    char* buffer_;
+};
---
+++ src/volume_class.cpp
@@ -0,0 +1,28 @@
+﻿#include "volume_class.hpp"
+
+// Exercise added to CMake executable files
+
+int AudioPlayer::volume() const { return volume_; }
+char AudioPlayer::buffer() const { return *buffer_; }
+
+AudioPlayer &AudioPlayer::operator=(const AudioPlayer &other) {
+    if (&other == this) return *this;
+
+    volume_ = other.volume_;
+    *buffer_ = *other.buffer_;
+    return *this;
+}
+
+AudioPlayer::~AudioPlayer() { delete[] buffer_; }
+
+bool AudioPlayer::set_volume(const int volume) {
+    if (volume < 0 || volume > 100) return false;
+    if (volume == volume_) return true;
+
+    volume_ = volume;
+    return true;
+}
+
+void AudioPlayer::max_volume() { volume_ = 100; }
+
+void AudioPlayer::mute() { volume_ = 0; }
