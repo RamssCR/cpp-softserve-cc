ce61e238bf6bb4c09d3f4a61126fdbe81bee64c2
RamssCR <ramsesgpm@gmail.com>
2026-02-18 07:47:53 -0500

    feat: created permutation exercise

---
+++ src/int_mutation.cpp
@@ -0,0 +1,20 @@
+﻿void swap(int* x, int* y) {
+    const int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void bet(int& x, int& y) {
+    if (x > y)
+        x = x * 2;
+    else
+        y = y * 2;
+}
+
+// TIP Check by using C++ debugger.
+int main() {
+    int x{20}, y{50};
+
+    bet(x, y);
+    swap(&x, &y);
+}
